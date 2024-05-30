19210f745553dbaa27e649f6b7d011a91685662a
kwrona05 <kwrona05@gmail.com>
2024-05-30 06:18:59 +0200

    compilate ts to script.js

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-// const spanOne = document.querySelector('.one')
-// const spantwo = document.querySelector('.two')
-
-// const input = document.querySelector('.input')
-// const paragraph = document.querySelector('.result')
-
-// const ConvButton = document.querySelector('.convert')
-// const resetButton = document.querySelector('.reset')
-// const swapButton = document.querySelector('.swap')
-
-
-// function convert() {
-//     if(spanOne.textContent === 'C') {
-//         let FDegree = input.value * 1.8 + 32
-//         let roundF = Math.round(FDegree * 10) / 10
-//         paragraph.textContent = `${roundF}`
-//     } else {
-//         let CDegree = (input.value - 32) * 0.55
-//         let roundC = Math.round(CDegree * 10) / 10
-//         paragraph.textContent = `${roundC}`
-//     }
-// }
-// function reset() {
-//     input.value = ''
-//     paragraph.textContent = ''
-// }
-
-// function swap() {
-//     if (spanOne.innerText === 'C') {
-//         spanOne.textContent = 'F'
-//         spantwo.textContent = 'C'
-//     } else {
-//         spanOne.textContent = 'C'
-//         spantwo.textContent = 'F'
-//     }
-// }
-
-
-
-// ConvButton.addEventListener('click', convert)
-// resetButton.addEventListener('click', reset)
-// swapButton.addEventListener('click', swap)
+var spanOne = document.querySelector('.one');
+var spantwo = document.querySelector('.two');
+var input = document.querySelector('.input');
+var paragraph = document.querySelector('.result');
+var ConvButton = document.querySelector('.convert');
+var resetButton = document.querySelector('.reset');
+var swapButton = document.querySelector('.swap');
+function convert() {
+    if (spanOne && spanOne.textContent === 'C') {
+        if (input) {
+            var inputValue = input.value;
+            var FDegree = parseFloat(inputValue) * 1.8 + 32;
+            var roundF = Math.round(FDegree * 10) / 10;
+            if (paragraph) {
+                paragraph.textContent = "".concat(roundF);
+            }
+        }
+    }
+    else {
+        if (input && paragraph) {
+            var inputValue = input.value;
+            var CDegree = (parseFloat(inputValue) - 32) * (5 / 9);
+            var roundC = Math.round(CDegree * 10) / 10;
+            paragraph.textContent = "".concat(roundC);
+        }
+    }
+}
+function reset() {
+    if (input && paragraph) {
+        input.value = '';
+        paragraph.textContent = '';
+    }
+}
+function swap() {
+    if (spantwo && spanOne && spanOne.innerText === 'C') {
+        spanOne.textContent = 'F';
+        spantwo.textContent = 'C';
+    }
+    else {
+        if (spanOne && spantwo) {
+            spanOne.textContent = 'C';
+            spantwo.textContent = 'F';
+        }
+    }
+}
+ConvButton === null || ConvButton === void 0 ? void 0 : ConvButton.addEventListener('click', convert);
+resetButton === null || resetButton === void 0 ? void 0 : resetButton.addEventListener('click', reset);
+swapButton === null || swapButton === void 0 ? void 0 : swapButton.addEventListener('click', swap);
